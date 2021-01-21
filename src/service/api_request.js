d3da20fcce94597838d4a1a14c90a08c08f7e4d2
project-team-ece <rahulkrishnanr2021@srishakthi.ac.in>
2021-01-21 06:18:34 +0530

    INIT

---
+++ src/service/api_request.js
@@ -0,0 +1,15 @@
+const API="http://localhost:8000";
+
+const fetchData=()=>{
+    return fetch(`${API}/fetch_data`,{
+        method:"GET",
+        headers:{
+            "Content-Type":"application.json"
+        }
+    }).then(response=> {
+       return  response.json()
+    })
+   . catch(()=>console.log("error"))
+}
+
+export default fetchData
